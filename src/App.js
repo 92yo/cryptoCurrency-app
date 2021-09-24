9d9644f88aa98551e98ed42cc0ba6d6b3981e7fe
yousefr <yousefrizik92@gmail.com>
2021-09-24 11:09:57 +0300

    adds initial file components. adds initial routes to components. adds navbar and footer design.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import "./app.css";
+import {
+  Navbar,
+  Exchanges,
+  HomePage,
+  CryptoCurrencies,
+  CryptoDetails,
+  CryptoNews,
+} from "./components";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/" render={HomePage} />
+              <Route path="/exchanges" render={Exchanges} />
+              <Route path="/cryptocurrencies" render={CryptoCurrencies} />
+              <Route path="/crypto/:coinId" render={CryptoDetails} />
+              <Route path="/news" render={CryptoNews} />
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/CryptoNews.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CryptoNews = () => {
+  return (
+    <div>
+      <h1>CryptoNews</h1>
+    </div>
+  );
+};
+
+export default CryptoNews;
